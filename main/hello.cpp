41adff651fce8dcd0ffd6a597f79d5e0e19e019f
T H Manoj <you@example.com>
2025-03-10 16:08:52 +0530

    mainn

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I PES2UG22CS612 have successfully built and run this  \n";
+  cout << "I (T H Manoj)PES2UG22CS612 have successfully built and run this and also edited this  \n";
+  cout << "Edited\n";
   return 0;
 }
